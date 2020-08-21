c17a34b9ece1b34909c96bb7caddb2b0d5736b03
rondevous <67483423+rondevous@users.noreply.github.com>
2020-08-21 10:28:05 +0530

    Simple XML replacer with in-code dictionary

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Telegram Translation Strings Replacer
-Rename strings in your Telegram langpack in one go.
+Rename strings in your Telegram langpack in one go. Unicode characters are supported.
 
-### The script is in a very early stage of development
-Currently, replacer.py only works with XML files and a single find-replace pair can be used at a time.
+## How to use
+1. Download and Install [Python 3](https://www.python.org/downloads_
+2. Download the [replacer.py](https://github.com/rondevous/tgStringsBulkReplace/raw/master/replacer.py) script.
+    - When the page loads, `right click > save as` to download it.
+3. Move the replacer.py and your language.XML to a new folder
+4. Open replacer.py in an editor, and edit the dictionary (right at the top)
+4. Open the folder in command prompt or terminal
+5. Enter this: `python replacer.py --file language.xml`
 
-Unicode is used, so replacements can be done regardless of which character-script you use.
+### The script is in early stage of development
+Currently, `replacer.py` only works with XML files.
 
-## Requirements
+Unicode is used to make replacements. That means you can find-replace non-latin characters present in russian, hindi, malayalam, etc
+
+### Requirements
 - Python3
-
--- replacer.py
+++ replacer.py
@@ -1,55 +1,44 @@
+# Replacing inside .strings file is currently not supported
+replace_dictionary = {
+	"groups":"potatoes",
+	"members":"chips",
+	"Restart bot":"Restart life",
+}
+
+# IMPORTANT!!! Do not change the code below
+
+keylist = list(replace_dictionary)
 # Imports
 import xml.etree.ElementTree as ET
 import re
 import argparse
 
-# **************************
-# List of parsers to pick for parsing the replacement list:
-""" https://tomassetti.me/parsing-in-python/ """
-
-# Some topics to think about:
-"""
-Lexer and Parser (using ply?)
-re
-enumerate
-"""
-# Some info so I don't get confused:
-"""
-Python List/Array = [kk,nnn,hhh]
-Python Dictionary = {
-	"groups":"potatoes"
-	"members":"chips"
-}
-"""
-# **************************
-
-
 # Command-line info and argument parsing
-#
 arg_parser = argparse.ArgumentParser(
 	description='Make a bulk rename to your languagePack.xml file')
 arg_parser.add_argument(
-	'--lang', '-l', metavar='LANG.XML', type=str, required=True,
+	'--file', '-f', metavar='LANGFILE.XML', type=str, required=True,
 	help='The exported language file (.xml) from the translations.telegram.org platform')
 # arg_parser.add_argument(
-# 	'--list', metavar='LIST.TXT', type=str, required=True,
+# 	'-l','--list', metavar='Replace_list.txt', type=str, required=True,
 #	help='File containing list of replacements to be make')
 arg_parser.add_argument(
 	'-p','--preview', action="store_true",
 	help='Show a preview of the results, but do not modify the language file.')
 
 args = arg_parser.parse_args()
-print("Using file:",args.lang)
+lang_file = str(args.file)  # str('android_lang_v1234567.xml')
+print("\nEditing file: '"+lang_file+"'")
 
+# log possibilites
+possibilites = open("possibilites.log","w")
 
 # Sample
 match_text = str('Admin')
 replace_text = str('Alpha')
-lang_file = str(args.lang)  # e.g: str('android_emoji_v923880.xml')
 
-
-# Initiate Information Variables
 global tree, total_cased, total_uncased
+# Initialise Variables
 total_cased = 0
 total_uncased = 0
 
@@ -73,46 +62,71 @@ if(isXML()):
 	global tree
 	# tree = ET.parse(lang_file)
 	root = tree.getroot()	# <Element 'resources'>
-
+	#
 	if(args.preview):
 		print('\n\nThese replacements will be made:')
-	
-	i=1
+	#
+	strCount=1
 	for string in root.findall('string'):
 		string_name = string.get('name')
-		translation = string.text
-		# To learn about the regex used below, type in the Python Console: 	help('re')
-		# Look for 'Special characters' and 'Functions' in the `re` module
-		# 
-		exact_replace = re.subn(r'(?<!\w)'+match_text+r'(?!\w)', replace_text, translation, re.UNICODE)
-		uncased_match = re.subn(r'(?<!\w)'+match_text.casefold()+r'(?!\w)', r'XXX', translation.casefold(), re.UNICODE)[1] - exact_replace[1]
-		
+		translation = str(string.text)
+		exact_subn = 0
+		uncased_subn = 0
+		for key in keylist:
+			if key is keylist[0]:
+				translation = (translation,0)
+			match_text = str(key)
+			replace_text = str(replace_dictionary[key])
+			# To learn about the regex used below, type in the Python Console: 	help('re')
+			translation = re.subn(r'(?<!\w)'+match_text+r'(?!\w)', replace_text, translation[0], re.UNICODE)
+			uncased_translation = re.subn(r'(?<!\w)'+match_text.casefold()+r'(?!\w)', 'DUMMY', translation[0], re.UNICODE, re.IGNORECASE)
+			uncased_subn += uncased_translation[1]
+			exact_subn += translation[1]
+		#end looping replacements
+		#
 		# Useful info
-		total_cased += exact_replace[1]
-		total_uncased += uncased_match
-
-		if(exact_replace[1] == 0):
+		total_cased += exact_subn
+		if(uncased_subn - exact_subn > 0):
+			total_uncased += uncased_subn - exact_subn
+			possibilites.write(str(translation[0])+'\n') # Log
+		#
+		if(exact_subn == 0):
+			root.remove(string)
 			continue
 		if(args.preview):
-			print('\n'+str(i)+'.',string_name)
-			print(exact_replace[0])
-		else:
-			string.text = exact_replace[0]
-		i+=1
-	
-	print('\nMade a total of',total_cased,'replacements in',(i-1),'strings.')
-	print('Found and ignored '+str(total_uncased)+' possible replacements (uncased).')
-	
-	if(args.preview):
-		print('\n[WARNING]: REPLACEMENTS ARE NOT WRITTEN TO THE FILE')
-	else:
-		newfile = str(re.sub(r'(.*)\.xml', r'\1_[EDITED].xml', lang_file))
-		tree.write(newfile, xml_declaration=True, encoding='Unicode')
-		print("\nHere's your file:\n\t",newfile)
-		print("\nImport it on the Translations Platform. Please review before importing all.")
-	
-	del root, tree, ET # free some memory
+			print('\n'+str(strCount)+'.',string_name)
+			print(translation[0])
+		string.text = translation[0]
+		strCount+=1
+	#end looping strings
+	possibilites.close()
+	#
+	newfile = str(re.sub(r'(.*)\.xml', r'[EDITED]_\1.xml', lang_file))
+	tree.write(newfile, xml_declaration=True, encoding='Unicode')
+	#
+	print('\nMade a total of',total_cased,'replacements in',(strCount-1),'strings.')
+	print("Here's the new file:\n\t'"+newfile+"'")
+	print("\nPlease check before importing it on the Translations Platform.")
+	#
+	if total_uncased != 0:
+		print('\nIgnored '+str(total_uncased)+' possible replacements (uncased).')
+		print('Check \'possibilites.log\', add them to the dictionary and re-run the script.')
+	#
+	del root, tree, ET # free some memory (XML)
 else:
-	print('\t',lang_file,' is not an XML file.')
-	print("Please use an XML file which is exported from translations.telegram.org")
-	print('(How to export: t.me/group/message)') #FIXME
+	print("\nERROR: '"+lang_file+"' is not an XML file.")
+	print("\nPlease use an XML file which is exported from translations.telegram.org")
+	print('How to export --> https://t.me/TranslationsTalk/1759)') #FIXME
+
+
+# ************* SOME THOUGHTS *************
+# List of parsers to pick for parsing the replacement list:
+""" https://tomassetti.me/parsing-in-python/ """
+
+# Some topics to think about:
+"""
+Lexer and Parser (using ply?)
+re
+enumerate
+"""
+# **************************
